97530b1443da94a2f1e0334b7048d03aaf329435
JohannesMP <joshadow@gmail.com>
2018-01-11 14:56:25 -0800

    added automatic browser opening to serve script

--- Serve.bat
+++ Serve.bat
@@ -1,18 +1,34 @@
 @echo off
 
-REM Use this script to serve the jekyll page locally for testing.
-REM Make sure to run 'bundle install' before running the first time.
+:: Use this script to serve the jekyll page locally for testing.
+:: Make sure to run 'bundle install' before running the first time.
 
 
-set jekyll_cmd=bundle exec jekyll serve --port=4444
+:: The port we will run jekyll's webserver on
+set port=4444
+:: The local hostname/IP to run jekyll's webserver on
+set host=127.0.0.1
 
-REM If run with double click, run in persistent window
-REM If run from cmd just run normally
+:: The command used to run jekyll serve
+set serve_cmd=jekyll serve
+:: Additional args to pass in
+set args=
+
+
+:: -------------------------------------------------
+:: Don't touch below
+
+set run_cmd=bundle exec %serve_cmd% --port=%port% --host=%host% %args%
+
+:: Open website first so we can block on jekyll serve
+:: NOTE: this will 404 temporarily until jekyll is up
+start http://%host%:%port%
+
+:: If run with double click, run in persistent window
+:: If run from cmd just run normally
 IF %0 == "%~0" (
-    start "JEKYLL" echo %jekyll_cmd% ^& %jekyll_cmd%
+    start "JEKYLL" echo %run_cmd% ^& %run_cmd%
 ) ELSE (                         
-                   echo %jekyll_cmd% && %jekyll_cmd%
+                   echo %run_cmd% && %run_cmd%
 )
-
-
 
