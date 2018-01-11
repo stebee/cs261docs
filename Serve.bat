d8e2d5a377f8c1ef7fe7a58599fc300e08288d56
Johannes Peter <joshadow@gmail.com>
2018-01-11 03:39:33 -0800

    updated serve.bat to behave nicer when running from commandline

--- Serve.bat
+++ Serve.bat
@@ -4,4 +4,15 @@ REM Use this script to serve the jekyll page locally for testing.
 REM Make sure to run 'bundle install' before running the first time.
 
 
-start "JEKYLL" bundle exec jekyll serve --port=4444
+set jekyll_cmd=bundle exec jekyll serve --port=4444
+
+REM If run with double click, run in persistent window
+REM If run from cmd just run normally
+IF %0 == "%~0" (
+    start "JEKYLL" echo %jekyll_cmd% ^& %jekyll_cmd%
+) ELSE (                         
+                   echo %jekyll_cmd% && %jekyll_cmd%
+)
+
+
+
